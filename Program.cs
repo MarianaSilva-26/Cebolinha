a96c8295b28f0a5a67ab79bdcd1b8551afa0a8f2
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-19 00:14:24 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String frase;
+            Console.Write("digite uma frase:");
+            frase = Console.ReadLine();
+            Console.Write("");
+            Console.WriteLine($"{frase.Replace("r" , "l").Replace("R" , "L")}");
+              //<---fim(digite)--->\\
+
+              //<---inicio(sair)--->\\
+            Console.Write("agora presssione esnter para finalisar:");
+            Console.ReadKey();
+            
+              
+            
+
+        }
+    }
+}
